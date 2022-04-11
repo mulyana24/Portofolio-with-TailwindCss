f6211e8041b4fcee4b5c8d264a09d6aba3d02d72
mulyana24 <49643139+mulyana24@users.noreply.github.com>
2022-04-11 22:53:31 +0700

    add fitur dark mode & fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,17 @@
-// Navbar Fixed
+// Navbar Fixed & back to-top
 window.onscroll = function() {
     const header = document.querySelector('header')
     const navFixed = header.offsetTop
+    const toTop = document.querySelector('#to-top')
 
     if(window.pageYOffset > navFixed) {
         header.classList.add('navbar-fixed')
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
     }else{
         header.classList.remove('navbar-fixed')
+        toTop.classList.add('hidden')
+        toTop.classList.remove('flex')
     }
 }
 
@@ -18,4 +23,32 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden')
 })
+
+// klik tombol di luar hamburger
+window.addEventListener('click', function (e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active')
+        navMenu.classList.add('hidden')
+    }
+})
+
+// Darkmode Toggle
+const modeToggle = document.querySelector('#darkMode-toggle')
+const html = document.querySelector('html')
+
+modeToggle.addEventListener('click', function(){
+    if(modeToggle.checked) {
+        html.classList.add('dark')
+        localStorage.theme = 'dark'
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light'
+    }
+})
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    modeToggle.checked = true
+    } else {
+    modeToggle.checked = false
+    }
 
